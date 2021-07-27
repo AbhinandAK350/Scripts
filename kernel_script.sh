d6b7045ec8c53cb1676561ecc6b73490f80316e8
Abhinand A K <abhinandak@outlook.com>
2021-07-27 21:10:10 +0530

    Update kernel_script.sh

--- kernel_script.sh
+++ kernel_script.sh
@@ -8,6 +8,7 @@ DEVICE=""
 KERNEL_NAME="perf"
 export ARCH="arm64"
 CONFIG="$DEVICE-perf_defconfig"
+BUILD_DTBO=true
 ZIPNAME="$KERNEL_NAME-$DEVICE-$(date '+%Y%m%d-%H%M').zip"
 
 # Color
@@ -17,59 +18,89 @@ GREEN='\033[0;32m'
 BLUE='\033[0;36m'
 NC='\033[0m'
 
-# Clang
-if ! [ -d "$HOME/proton-clang" ]; then
-	echo "${YELLOW}Proton clang not found! Cloning...${NC}"
-	if ! git clone -q --depth=1 --single-branch https://github.com/kdrag0n/proton-clang ~/proton; then
-		echo -e "${RED}Cloning failed! Aborting...${NC}"
-		exit 1
+# Get tools
+if [[ $1 == "-t" || $1 == "--tools" ]]; then
+	echo -e "\n${YELLOW}Installing tools...${NC}\n"
+	sudo apt install bc bash gnupg build-essential zip curl make automake autogen autoconf autotools-dev \
+	libtool shtool python m4 gcc libtool zlib1g-dev flex bison libssl-dev
+
+	# Clang
+	if ! [ -d "$HOME/proton-clang" ]; then
+        	echo -e "\n${YELLOW}Proton clang not found! Cloning...${NC}\n"
+	        if ! git clone -q --depth=1 --single-branch https://github.com/kdrag0n/proton-clang ~/proton; then
+        	        echo -e "\n${RED}Cloning failed! Aborting...${NC}\n"
+	        fi
+	else
+		echo -e "\n${BLUE}Clang found!${NC}\n"
 	fi
+
+	# Anykernel3
+	if ! [ -d "AnyKernel3" ]; then
+		echo -e "\n${YELLOW}Proton clang not found! Cloning...${NC}\n"
+                git clone -q https://github.com/AbhinandAK350/AnyKernel3 -b $DEVICE
+	else
+		echo -e "\n${BLUE}AnyKernel3 found!${NC}\n"
+        fi
+
+	echo -e "\n${GREEN}Tools installed successfuly!{NC}\n"
+	exit 1
 fi
 
 # Compile
-if [[ $1 == "-m" || $1 == "--miui" ]]; then echo -e "\n${YELLOW}Starting compilation... (MIUI)${NC}\n" ; else echo -e "\n${YELLOW}Starting compilation...${NC}\n" ; fi
-if [ $ARCH == "arm64" ]; then IMAGE="Image.gz-dtb" ; elif [ $ARCH == "arm" ]; then IMAGE="zImage-dtb" ; fi
+if [[ $1 == "-m" || $1 == "--miui" ]]; then
+	echo -e "\n${YELLOW}Starting compilation... (MIUI)${NC}\n"
+else
+	echo -e "\n${YELLOW}Starting compilation...${NC}\n"
+fi
+
+if [ $ARCH == "arm64" ]; then
+	IMAGE="Image.gz-dtb"
+elif [ $ARCH == "arm" ]; then
+	IMAGE="zImage-dtb"
+fi
+
 echo -e "\t${BLUE}DEVICE = $DEVICE"
 echo -e "\tARCH = $ARCH"
-echo -e "\tCONFIG = $CONFIG${NC}\n"
+echo -e "\tCONFIG = $CONFIG"
+if [ $BUILD_DTBO == true ]; then
+	echo -e "\tDTBO included ${NC}\n"
+else
+	echo -e "${NC}"
+fi
 mkdir -p out
 make O=out $CONFIG
-ccache make -j$(nproc --all) O=out ARCH=arm64 CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- $IMAGE dtbo.img
+make -j$(nproc --all) O=out ARCH=arm64 CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- $IMAGE dtbo.img
 
 # Packing
-if [ -f "out/arch/$ARCH/boot/Image.gz-dtb" ] && [ -f "out/arch/$ARCH/boot/dtbo.img" ]; then
+if [ -f "out/arch/$ARCH/boot/Image.gz-dtb" ] || [ -f "out/arch/$ARCH/boot/dtbo.img" ]; then
 	echo -e "\n${GREEN}Kernel compiled succesfully! Zipping up...${NC}\n"
-	if ! [ -d "AnyKernel3" ]; then
-		git clone -q https://github.com/AbhinandAK350/AnyKernel3 -b $DEVICE
-	fi
-
-	if [[ $1 == "-m" || $1 == "--miui" ]]; then
+		if [[ $1 == "-m" || $1 == "--miui" ]]; then
 		# For MIUI
 		# Credit Adek Maulana <adek@techdro.id>
 		OUTDIR="$PWD/out/"
 		VENDOR_MODULEDIR="$PWD/AnyKernel3/modules/vendor/lib/modules"
-
 		STRIP="$HOME/proton-clang/aarch64-linux-gnu/bin/strip$(echo "$(find "$HOME/proton-clang/bin" -type f -name "aarch64-*-gcc")" | awk -F '/' '{print $NF}' |\
 				sed -e 's/gcc/strip/')"
 
 		for MODULES in $(find "${OUTDIR}" -name '*.ko'); do
 			"${STRIP}" --strip-unneeded --strip-debug "${MODULES}"
 			"${OUTDIR}"/scripts/sign-file sha512 \
-					"${OUTDIR}/certs/signing_key.pem" \
-					"${OUTDIR}/certs/signing_key.x509" \
-					"${MODULES}"
+			"${OUTDIR}/certs/signing_key.pem" \
+			"${OUTDIR}/certs/signing_key.x509" \
+			"${MODULES}"
 			find "${OUTDIR}" -name '*.ko' -exec cp {} "${VENDOR_MODULEDIR}" \;
 
 			case ${MODULES} in
 					*/wlan.ko)
 				cp "${MODULES}" "${VENDOR_MODULEDIR}/pronto_wlan.ko" ;;
 			esac
-
 		done
-	fi
+fi
 
 	cp out/arch/$ARCH/boot/Image.gz-dtb AnyKernel3
-	cp out/arch/$ARCH/boot/dtbo.img AnyKernel3
+	if [ $BUILD_DTBO == true ] &&  [ -f out/arch/$ARCH/boot/dtbo.img ]; then
+		cp out/arch/$ARCH/boot/dtbo.img AnyKernel3
+	fi
 	rm -f *zip
 	cd AnyKernel3
 	rm -f *zip
